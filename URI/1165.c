164a2a99dd533097b80c6115fbcecbf924ff0310
Lucas Kaczynski Primon <lucaskaczyn@gmail.com>
2021-07-15 18:25:18 -0300

    --allow-empty

--- URI/1165.c
+++ URI/1165.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+typedef enum { false, true } bool;
+
+bool isDivider(int number, int i) {
+	if(number % i == 0)
+		return true;
+	
+	return false;
+}
+
+bool isPrime(int number) {
+	int count = 0;
+
+	for(int i = 1; i <= number; i++) {
+		if(isDivider(number, i))
+			count++;
+	}
+
+	if(count == 2)
+		return true;
+	
+	return false;
+}
 
 int main() {
 	int n;
 	scanf("%d", &n);
 
 	for(int i = 0; i < n; i++) {
-		printf("%d\n", i);
+		int number;
+
+		scanf("%d", &number);
+
+		if(isPrime(number))
+			printf("%d eh primo\n", number);
+		else
+			printf("%d nao eh primo\n", number);
 	}
+
+	return 0;
 }
